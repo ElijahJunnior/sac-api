a2133180a65d6d5f9a80d45b529f240661a53c3b
Elias Junior <elijah.junnior@outlook.com>
2021-11-12 14:24:29 -0300

    Testing Connection archive

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,8 +1,26 @@
-import sql from 'mssql';
+import { connect, ConnectionPool } from 'mssql';
 
-export async function openConnection() {
+let FIN: ConnectionPool | undefined, SAC: ConnectionPool | undefined
 
-    const sqlConfig = {
+function getSACConnection(): ConnectionPool {
+    if (SAC) {
+        return <ConnectionPool>SAC;
+    } else {
+        throw new Error('SAC connection is undefined!');
+    }
+}
+
+function getFINConnection(): ConnectionPool {
+    if (FIN) {
+        return <ConnectionPool>FIN;
+    } else {
+        throw new Error('FIN connection is undefined!');
+    }
+}
+
+async function openConnection() {
+
+    const SACConfig = {
         user: process.env.DB_USER ?? '',
         password: process.env.DB_PASSWORD ?? '',
         database: process.env.DB_DATABASE ?? '',
@@ -19,15 +37,51 @@ export async function openConnection() {
         }
     }
 
+    const FINConfig = {
+        user: process.env.DB_USER ?? '',
+        password: process.env.DB_PASSWORD ?? '',
+        database: process.env.DB_DATABASE ?? '',
+        server: process.env.DB_SERVER ?? '',
+        port: Number(process.env.DB_PORT ?? 0),
+        pool: {
+            max: 10,
+            min: 0,
+            idleTimeoutMillis: 30000
+        },
+        options: {
+            encrypt: false, // for azure
+            trustServerCertificate: false, // change to true for local dev / self-signed certs
+        }
+    }
+
+
     try {
         // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect(sqlConfig)
-        console.log('Connection opened successfully')
+        SAC = await connect(SACConfig);
+        console.log('Connection on SAC database opened successfully')
     } catch (err) {
-        console.log('Erro to open connection: ', err)
-        // ... error checks
+        console.log('Erro to open connection on SAC database: ', err)
+    }
+
+    try {
+        // make sure that any items are correctly URL encoded in the connection string
+        FIN = await connect(FINConfig);
+        console.log('Connection on Financeiro database opened successfully')
+    } catch (err) {
+        console.log('Erro to open connection on Financeiro database: ', err)
     }
 
 }
 
-export { sql };
+
+type ConnectionProps = {
+    FIN: () => ConnectionPool,
+    SAC: () => ConnectionPool,
+    open: () => Promise<void>
+}
+
+export const Connection: ConnectionProps = {
+    FIN: getFINConnection,
+    SAC: getSACConnection,
+    open: openConnection,
+}
